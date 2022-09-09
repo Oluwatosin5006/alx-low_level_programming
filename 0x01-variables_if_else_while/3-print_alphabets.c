f707cebd392b5b4654790d021416e18fffbabe1a
Oluwatosin5006 <jamesoluwatosinfadeyi@gmail.com>
2022-09-09 15:34:34 -0700

    Task 2 - 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+
+
+int main(void)
+
+{
+
+	int n;
+
+	return (0);
+
+}
+chmod u+x 3-print_alphabets.c
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+
+
+int main(void)
+
+{
+
+	int n;
+
+	return (0);
+
+}
+
+
